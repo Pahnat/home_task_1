bb7931842b8de72d5ae5745df4951ef8805ed755
Pavlo-Hnatenko <gnatenko.pavlo@gmail.com>
2020-06-06 11:14:14 +0300

    third commit

--- src/ChessRook.java
+++ src/ChessRook.java
@@ -1,2 +1,31 @@
 public class ChessRook {
+
+    private static int x1;
+    private static int y1;
+    private static int x2;
+    private static int y2;
+
+    public static void main(String[] args) {
+
+        x1 = 1;
+        y1 = 1;
+        x2 = 2;
+        y2 = 2;
+        isBeat();
+
+        x1 = 1;
+        y1 = 1;
+        x2 = 2;
+        y2 = 1;
+        isBeat();
+
+    }
+
+    private static void isBeat (){
+
+        if ((x1 == x2) || (y1 == y2)){
+            System.out.println("YES");
+        } else
+            System.out.println("NO");
+    }
 }
--- src/ThreeNumbers.java
+++ src/ThreeNumbers.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class ThreeNumbers {
+
+//    this code isn't ready
+
+/*
+    private static int x1;
+    private static int x2;
+    private static int x3;
+    private static int[] ints = new int[3];
+
+    public static void main(String[] args) {
+
+        x1 = 1;
+        x2 = 2;
+        x3 = 3;
+        for ()
+
+        System.out.println(Arrays.toString(ints));
+        isEqual();
+
+
+
+
+    }
+
+    private static void isEqual(){
+        int num = 0;
+        for(int i = 0; i < ints.length; i++){
+            for(int j = 1; j < ints.length; j++){
+                if(ints[i] == ints[j]){
+                    num++;
+                    break;
+                }
+            }
+        }
+        System.out.println(num);
+    }
+
+ */
 }
